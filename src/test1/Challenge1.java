919b48702279a472510b1f4843b3984e78bcee22
ManasaKomali <komalimanasa@gmail.com>
2025-07-02 20:34:04 +0530

    1

--- src/test1/Challenge1.java
+++ src/test1/Challenge1.java
@@ -1,4 +1,15 @@
 package test1;
 
 public class Challenge1 {
+    public static void main(String[] args) {
+        int number=50;
+        boolean isTrue=true;
+        double decimalNumber= 60.45;
+        char ch = 'A';
+        System.out.println("Integer: "+number);
+        System.out.println("Double: "+decimalNumber);
+        System.out.println("Boolean: "+isTrue);
+        System.out.println("Character: "+ch);
+
+    }
 }
